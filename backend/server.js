a58ea61c31fe0a256b12401424344e7f2a02e9a6
Kumar Raj <kumarrajsingh585@gmail.com>
2026-01-09 17:14:54 +0530

    feat: updated configs for server

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-connectDB()
+connectDB();
 
-module.exports = app; 
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
